46202542bf1a4be425e33b1cbd28012e4dcf138d
0xack13 <Saleh@0xack13s-MacBook-Air.local>
2017-07-15 00:23:07 +1000

    Initial commit

---
+++ mongoWireProtoConn.py
@@ -0,0 +1,14 @@
+from struct import *
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("127.0.0.1", 27017))
+
+message = bytearray()
+testMsg1 = "HELLO!".encode('utf-8')
+testMsg2 = bytes.fromhex("00")
+message.extend(pack('<4shhhhh',testMsg1,0,16+len(testMsg1),1,0,1000))
+
+s.send(message)
+
+s.close()
