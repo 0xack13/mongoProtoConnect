a82bd9bc2ede3e4727c5ae51b1cc5989066c4ea7
0xack13 <Saleh@0xack13s-MacBook-Air.local>
2017-07-15 13:02:02 +1000

    Working

--- mongoWireProtoConn.py
+++ mongoWireProtoConn.py
@@ -1,15 +1,17 @@
 from struct import *
 import socket
 
+buffer_size=1024
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 27017))
 
 message = bytearray()
-testMsg1 = "HELLO!".encode('utf-8')
-testMsg2 = bytes.fromhex("00")
-#mongodb uses little-endian
-message.extend(pack('<4shhhhh',testMsg1,0,16+len(testMsg1),1,0,1000))
+ss = bytes("Where is Mongo?", 'utf-8')    # Or other appropriate encoding
+message.extend(pack("<iiii16sh", 16+len(ss)+1,1,0,1000, ss, 0))
 
 s.send(message)
+data = s.recv(buffer_size)
+print(data)
 
 s.close()
